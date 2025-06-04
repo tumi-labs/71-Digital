eeb775caac8f417c81c9679c3ecfc910c139f833
MoizYousuf <moizyousuf24@gmail.com>
2025-06-04 19:46:23 +0500

    Comment out host binding in server listen for clarity and future reference

--- server/index.ts
+++ server/index.ts
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   const port = 5000;
   server.listen({
     port,
-    host: "0.0.0.0",
+    // host: "0.0.0.0",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);

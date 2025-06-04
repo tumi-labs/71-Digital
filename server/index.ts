189da18a6345d6d727dcc42c6952e08fc14dc668
MoizYousuf <moizyousuf24@gmail.com>
2025-06-04 20:00:30 +0500

    Refactor vercel.json to streamline routes configuration and remove unnecessary line breaks

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
